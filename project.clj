b9322525ecc528b533c27954474a38359c50c488
Polawat Phetra <pphetra@gmail.com>
2010-10-31 12:08:05 +0700

    add selenium dependency. (use "cake deps" to fetch dependencies jar)

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [congomongo "0.1.3-SNAPSHOT"]])
+		 [congomongo "0.1.3-SNAPSHOT"]
+		 [org.seleniumhq.selenium/selenium "2.0a6"]])
